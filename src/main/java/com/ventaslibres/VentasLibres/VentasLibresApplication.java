7840fb1d59e814f2564409e0e5a0b2c5a6c3cbb9
deivi ocampo <deibi.ocampo@sofka.com.co>
2020-05-23 17:49:14 -0500

    primer commit

---
+++ src/main/java/com/ventaslibres/VentasLibres/VentasLibresApplication.java
@@ -0,0 +1,13 @@
+package com.ventaslibres.VentasLibres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasLibresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasLibresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventaslibres/VentasLibres/VentasLibresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventaslibres.VentasLibres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasLibresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
